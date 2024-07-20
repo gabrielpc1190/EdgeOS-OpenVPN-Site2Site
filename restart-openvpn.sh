81cf52cf5c355ee849bcae35d9d686f03756b0c5
Gabriel Paniagua Castro <gabrielpc1190@gmail.com>
2024-07-20 07:28:30 -0600

    Update restart-openvpn.sh

--- restart-openvpn.sh
+++ restart-openvpn.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #ping the remote host
-/bin/ping -c 2 -W 2 192.168.253.2 1>/dev/null 2>&1
+/bin/ping -c 2 -W 2 10.1.0.1 1>/dev/null 2>&1
 if [ $? -ne 0 ]; then
-    logger -t openvpn "Host 192.168.253.2 stopped responding. Restarting VPN..."
+    logger -t openvpn "Host 10.1.0.1 stopped responding. Restarting VPN..."
     # Disable the vtun1 interface
-    sudo ip link set vtun1 down
-    logger -t openvpn "vtun1 interface disabled"
+    sudo ip link set vtun0 down
+    logger -t openvpn "vtun0 interface disabled"
     # Wait for a few seconds
     sleep 5
 
     # Enable the vtun1 interface
-    sudo ip link set vtun1 up
-    logger -t openvpn "vtun1 interface enabled"
-    logger -t openvpn "VPN BA restarted"
+    sudo ip link set vtun0 up
+    logger -t openvpn "vtun0 interface enabled"
+    logger -t openvpn "VPN restarted"
 fi
