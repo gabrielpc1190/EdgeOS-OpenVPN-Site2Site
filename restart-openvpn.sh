196c4c8f87b47d311a9c5c1ad88f4a39451cb8b6
Gabriel Paniagua Castro <gabrielpc1190@gmail.com>
2024-07-20 07:27:37 -0600

    Update restart-openvpn.sh
    Added the ability to disable and enable the interface in the configuration instead of messing around with the openvpn service.

--- restart-openvpn.sh
+++ restart-openvpn.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-/bin/ping -c 2 -W 2 10.0.0.1 1>/dev/null 2>&1
+#ping the remote host
+/bin/ping -c 2 -W 2 192.168.253.2 1>/dev/null 2>&1
 if [ $? -ne 0 ]; then
-       logger -t openvpn "VPN Failed. Restarting..."
-       killall -s SIGHUP openvpn
-	   logger -t openvpn REYCOM VPN restarted
+    logger -t openvpn "Host 192.168.253.2 stopped responding. Restarting VPN..."
+    # Disable the vtun1 interface
+    sudo ip link set vtun1 down
+    logger -t openvpn "vtun1 interface disabled"
+    # Wait for a few seconds
+    sleep 5
+
+    # Enable the vtun1 interface
+    sudo ip link set vtun1 up
+    logger -t openvpn "vtun1 interface enabled"
+    logger -t openvpn "VPN BA restarted"
 fi
